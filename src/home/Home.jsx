9501fbd2ef5eb7f3b21f762c86f468454899db54
sweedalgharshi <sweedalgracius@gmail.com>
2023-03-22 16:28:09 +0530

    initial commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <AcUnitIcon />
+    </div>
+  );
+};
+
+export default Home;

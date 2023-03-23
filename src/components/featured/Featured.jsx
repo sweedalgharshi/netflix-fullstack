f6b774e8520c831c2273da1d21ef4710f579e11e
sweedalgharshi <sweedalgracius@gmail.com>
2023-03-23 13:08:23 +0530

    added featured components with styling and options for movies and series

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,12 +1,55 @@
 import "./Featured.scss";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
-const Featured = () => {
+const Featured = ({ type }) => {
   return (
     <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
       <img
         src="https://images.pexels.com/photos/1819483/pexels-photo-1819483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="profile-pic"
       />
+      <div className="info">
+        <img
+          src="https://m.media-amazon.com/images/S/pv-target-images/6be452c4a997e7ef3ee85bee77ae07c02298f3491e4fa3ff91b56fb79b993745._UR1920,1080_SX356_FMwebp_.jpg"
+          alt=""
+        />
+        <span className="desc">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero at, obcaecati quae
+          voluptas quo vel? Tempore sit accusantium reprehenderit corrupti.
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrowIcon />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlinedIcon />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };

0f1a45b19f780110646ddfd85643dcf73dac8d79
sweedalgharshi <sweedalgracius@gmail.com>
2023-03-24 16:21:39 +0530

    done with homepage with styling and some functionality

--- src/components/list-item/ListItem.jsx
+++ src/components/list-item/ListItem.jsx
@@ -1,9 +1,54 @@
 import "./ListItem.scss";
 
-import React from "react";
+import React, { useState } from "react";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import AddIcon from "@mui/icons-material/Add";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
 
-function ListItem() {
-  return <div className="list-item">Item</div>;
+function ListItem({ index }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
+  return (
+    <div
+      className="list-item"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+    >
+      <img
+        src="https://m.media-amazon.com/images/S/pv-target-images/dbba13f2b8e065868db8014ce1166fc3452ed49aa9d485be95677f275b9f95cd._UR1920,1080_SX356_FMwebp_.jpg"
+        alt=""
+      />
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="item-info">
+            <div className="icons">
+              <PlayArrowIcon className="icon" />
+              <AddIcon className="icon" />
+              <ThumbUpOutlinedIcon className="icon" />
+              <ThumbDownOutlinedIcon className="icon" />
+            </div>
+
+            <div className="item-info-top">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>2019</span>
+            </div>
+
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum laborum ut aut
+              reiciendis. Dolorem at in maxime?
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ListItem;
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,18 +1,24 @@
 import "./List.scss";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 import ListItem from "../list-item/ListItem";
 
 function List() {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
   const listRef = useRef();
+
   function handleClick(direction) {
+    setIsMoved(true);
     let distance = listRef.current.getBoundingClientRect().x - 50;
-    if (direction === "left") {
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
     }
-    if (direction === "right") {
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
   }
@@ -23,18 +29,19 @@ function List() {
         <ArrowBackIosNewOutlinedIcon
           className="slider-arrow left"
           onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
         />
         <div className="container" ref={listRef}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
         </div>
         <ArrowForwardIosOutlinedIcon
           className="slider-arrow right"

5fcb8495edd4eca3c7677ea636b8fd96b2bca757
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-03 15:14:35 +0530

    react netflix watch page LINK functionality

--- client/src/components/list-item/ListItem.jsx
+++ client/src/components/list-item/ListItem.jsx
@@ -1,6 +1,7 @@
 import "./ListItem.scss";
 
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import AddIcon from "@mui/icons-material/Add";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
@@ -33,37 +34,47 @@ function ListItem({ index, item }) {
   //   "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
 
   return (
-    <div
-      className="list-item"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
-    >
-      <img src={movie.img} alt="" />
-      {isHovered && (
-        <>
-          <video src={movie.trailer} autoPlay={true} loop />
-          <div className="item-info">
-            <div className="icons">
-              <PlayArrowIcon className="icon" />
-              <AddIcon className="icon" />
-              <ThumbUpOutlinedIcon className="icon" />
-              <ThumbDownOutlinedIcon className="icon" />
-            </div>
+    <Link to="/watch" state={{ movie: movie }}>
+      <div
+        className="list-item"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      >
+        <img src={movie.img} alt="" />
+        {isHovered && (
+          <>
+            <video src={movie.trailer} autoPlay={true} loop />
+            <div className="item-info">
+              <div className="icons">
+                <PlayArrowIcon className="icon" />
+                <AddIcon className="icon" />
+                <ThumbUpOutlinedIcon className="icon" />
+                <ThumbDownOutlinedIcon className="icon" />
+              </div>
 
-            <div className="item-info-top">
-              <span>{item.duration}</span>
-              <span className="limit">+{movie.limit}</span>
-              <span>{movie.year}</span>
-            </div>
+              <div className="item-info-top">
+                <span>{item.duration}</span>
+                <span className="limit">+{movie.limit}</span>
+                <span>{movie.year}</span>
+              </div>
 
-            <div className="desc">{movie.desc}</div>
-            <div className="genre">{movie.genre}</div>
-          </div>
-        </>
-      )}
-    </div>
+              <div className="desc">{movie.desc}</div>
+              <div className="genre">{movie.genre}</div>
+            </div>
+          </>
+        )}
+      </div>
+    </Link>
   );
 }
 
 export default ListItem;
+
+// <Link to="/watch" state={{ movie: movie }}>
+// <Link
+//       to={{
+//         pathname: "/watch",
+//         movie: movie,
+//       }}
+//     ></Link>
